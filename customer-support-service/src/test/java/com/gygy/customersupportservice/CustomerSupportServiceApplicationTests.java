d64f96e4804c067afa1ec346cd51059e0eb5b4bc
Umut <umut.gu@outlook.com>
2025-03-05 21:39:05 +0300

    first

---
+++ customer-support-service/src/test/java/com/gygy/customersupportservice/CustomerSupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gygy.customersupportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/src/main/java/com/gygy/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gygy.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
