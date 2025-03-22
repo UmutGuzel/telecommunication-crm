3a492d5cc66af6472830c389157ff601fc40077f
Umut <umut.gu@outlook.com>
2025-03-22 11:37:44 +0300

    Refactor user service password reset functionality and update configurations
    - Updated Docker Compose to change PostgreSQL volume reference.
    - Modified GitHub Actions workflow to limit triggers to specific branches and paths.
    - Changed config server Git URI to use HTTP and added search paths for services.
    - Added new ResetPasswordCommand and ResetPasswordResponse classes for handling password resets.
    - Implemented reset password logic in UserController and associated command handler.
    - Introduced ApplicationConfig for managing application-specific configurations.
    - Created PasswordResetEvent class for event handling.
    - Updated application.yaml to include new configuration properties.

--- user-service/src/main/java/com/gygy/userservice/persistance/UserRepository.java
+++ user-service/src/main/java/com/gygy/userservice/persistance/UserRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.gygy.userservice.entity.User;
 import java.util.UUID;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByResetToken(String resetToken);
 }
