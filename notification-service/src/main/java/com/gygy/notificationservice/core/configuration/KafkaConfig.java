26bd807a24fb929c0fa0f277a05d84e771b28e28
Glcan22 <103893419+Glcan22@users.noreply.github.com>
2025-04-12 21:26:34 +0300

    Producer and NotificationPreferences added

--- notification-service/src/main/java/com/gygy/notificationservice/core/configuration/KafkaConfig.java
+++ notification-service/src/main/java/com/gygy/notificationservice/core/configuration/KafkaConfig.java
@@ -5,15 +5,17 @@ import com.gygy.common.events.UserActivationEvent;
 import com.gygy.notificationservice.tempdto.*;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,10 +30,12 @@ public class KafkaConfig {
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
 
+    // Consumer Configurations
     private <T> ConsumerFactory<String, T> createConsumerFactory(Class<T> clazz) {
         JsonDeserializer<T> deserializer = new JsonDeserializer<>(clazz);
         deserializer.addTrustedPackages("*");
         deserializer.setUseTypeMapperForKey(true);
+        deserializer.setRemoveTypeHeaders(false);
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -62,6 +66,22 @@ public class KafkaConfig {
     @Bean public ConcurrentKafkaListenerContainerFactory<String, CorporateCustomerUpdatedEvent> corporateCustomerUpdatedKafkaListenerContainerFactory() { return createFactory(CorporateCustomerUpdatedEvent.class); }
     @Bean public ConcurrentKafkaListenerContainerFactory<String, PlanCampaignEvent> planCampaignKafkaListenerContainerFactory() { return createFactory(PlanCampaignEvent.class); }
 
+    // Producer Configurations
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    // Topic Definitions
     @Bean public NewTopic passwordResetEventsTopic(@Value("${kafka.topics.passwordResetEventsTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
     @Bean public NewTopic userActivationEventsTopic(@Value("${kafka.topics.userActivationEventsTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
     @Bean public NewTopic contractDetailCreatedTopic(@Value("${kafka.topics.contractDetailCreatedTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
@@ -77,4 +97,5 @@ public class KafkaConfig {
     @Bean public NewTopic corporateCustomerCreatedTopic(@Value("${kafka.topics.corporateCustomerCreatedTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
     @Bean public NewTopic corporateCustomerUpdatedTopic(@Value("${kafka.topics.corporateCustomerUpdatedTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
     @Bean public NewTopic planCampaignTopic(@Value("${kafka.topics.planCampaignTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
+    @Bean public NewTopic notificationPreferencesChangedTopic(@Value("${kafka.topics.notificationPreferencesChangedTopic}") String topic) { return new NewTopic(topic, 1, (short) 1); }
 }
